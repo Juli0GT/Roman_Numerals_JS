d2c25561fbb82e58ac1f8f97742344d8e34a68ae
Juli0GT <julio.gteran@gmail.com>
2019-02-02 22:12:16 +0100

    Adds 5 test cases that maps decimal numbers (5, 10, 50, 100 and 500) to their corresponding roman numerals (V, X, L, C and D)

--- romans.test.js
+++ romans.test.js
@@ -4,3 +4,28 @@ test('converts 1 to I', () => {
   const decimal = 1;
   expect(romans(decimal)).toBe('I');
 });
+
+test('converts 5 to V', () => {
+  const decimal = 5;
+  expect(romans(decimal)).toBe('V');
+});
+
+test('converts 10 to X', () => {
+  const decimal = 10;
+  expect(romans(decimal)).toBe('X');
+});
+
+test('converts 50 to L', () => {
+  const decimal = 50;
+  expect(romans(decimal)).toBe('L');
+});
+
+test('converts 100 to C', () => {
+  const decimal = 100;
+  expect(romans(decimal)).toBe('C');
+});
+
+test('converts 500 to D', () => {
+  const decimal = 500;
+  expect(romans(decimal)).toBe('D');
+});
